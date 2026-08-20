1d22567f3b4079fccde92df3eb315ff742c002a2
vernersdev <vernersdev@gmail.com>
2026-08-20 09:34:25 +0300

    header adjustment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Navbar } from './core/layout/navbar/navbar';
+import { Header } from './core/layout/header/header';
 import { Footer } from './core/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Navbar,
+    Header,
     Footer,
   ],
   templateUrl: './app.html',
